d33abe95686410d3db62fd9870ec66ddaabbce9e
Lucas Roabert <lucasrobaert@hotmail.com>
2021-06-12 15:32:24 -0300

    passo 4.3

--- src/screens/App.js
+++ src/screens/App.js
@@ -1,28 +1,51 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, SafeAreaView, View} from 'react-native';
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.bigBlue}>Big Blue</Text>
-      <Text style={styles.samllRed}>Small Red</Text>
-    </View>
+    <SafeAreaView testID="main" style={styles.container}>
+      <View testID="first" style={styles.first}>
+        <View style={styles.subView} />
+        <View style={styles.subView} />
+        <View style={styles.subView} />
+      </View>
+
+      <View testID="second" style={styles.second}>
+        <View style={styles.subView} />
+        <View style={styles.subView} />
+        <View style={styles.subView} />
+      </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    flexDirection: 'column',
   },
-  bigBlue: {
-    color: 'blue',
-    fontSize: 50,
+  first: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'flex-start',
+    margin: 40,
+    borderColor: 'red',
+    borderWidth: 1,
   },
-  samllRed: {
-    color: 'red',
-    fontSize: 20,
+  second: {
+    flex: 2,
+    flexDirection: 'column',
+    justifyContent: 'space-evenly',
+    alignItems: 'flex-end',
+    margin: 40,
+    borderColor: 'red',
+    borderWidth: 1,
+  },
+  subView: {
+    height: 50,
+    width: 50,
+    backgroundColor: 'skyblue',
   },
 });
 
