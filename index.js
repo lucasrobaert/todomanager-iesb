9330efe843b2d4b1728913cd417540c9acf6a86f
Lucas Roabert <lucasrobaert@hotmail.com>
2021-06-12 16:02:45 -0300

    passo 4.5.1

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import {AppRegistry} from 'react-native';
 import App from './src/screens/App';
 import Login from './src/screens/Login';
+import Register from './src/screens/Register';
 import {name as appName} from './app.json';
 
 const Index = () => {
   return <Login email="lucas@robaert.com" />;
 };
 
-AppRegistry.registerComponent(appName, () => Index);
+AppRegistry.registerComponent(appName, () => Register);
